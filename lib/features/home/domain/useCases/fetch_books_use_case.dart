212a696d23a1eeec5ed3942eaf182aaa8257774a
SaifAllahGharib <saifallahgharib@gmail.com>
2024-10-06 13:33:20 +0300

    Create use case class

--- lib/features/home/domain/useCases/fetch_books_use_case.dart
+++ lib/features/home/domain/useCases/fetch_books_use_case.dart
@@ -1,14 +1,16 @@
 import 'package:book_app_clean_arch/core/errors/failure.dart';
+import 'package:book_app_clean_arch/core/useCases/use_case.dart';
 import 'package:book_app_clean_arch/features/home/domain/entities/BookEntity.dart';
 import 'package:book_app_clean_arch/features/home/domain/repositories/home_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchBooksUseCase {
+class FetchBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepository homeRepository;
 
   FetchBooksUseCase({required this.homeRepository});
 
-  Future<Either<Failure, List<BookEntity>>> fetchBooks({required String path}) {
-    return homeRepository.fetchBooks(path: path);
+  @override
+  Future<Either<Failure, List<BookEntity>>> call({required String path}) async {
+    return await homeRepository.fetchBooks(path: path);
   }
 }
