c387d3e0e899b7cddbf535396cbf9f3806e86f5d
SaifAllahGharib <saifallahgharib@gmail.com>
2024-10-08 14:09:41 +0300

    Add & implement bottom books cubit & state

---
+++ lib/features/home/presentation/manager/bottom_books_cubit/bottom_books_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:book_app_clean_arch/constants.dart';
+import 'package:book_app_clean_arch/core/errors/failure.dart';
+import 'package:book_app_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:book_app_clean_arch/features/home/domain/useCases/fetch_books_use_case.dart';
+import 'package:dartz/dartz.dart';
+
+import 'bottom_books_state.dart';
+
+class BottomBooksCubit extends Cubit<BottomBooksState> {
+  final FetchBooksUseCase fetchBooksUseCase;
+
+  BottomBooksCubit({required this.fetchBooksUseCase})
+      : super(BottomBooksInit());
+
+  Future<void> fetchBooks() async {
+    emit(BottomBooksLoading());
+    Either<Failure, List<BookEntity>> result =
+        await fetchBooksUseCase.call(path: endPointBottomBooks);
+
+    result.fold(
+      (failure) => emit(BottomBooksFailure(msg: failure.msg)),
+      (books) => emit(BottomBooksSuccess(books: books)),
+    );
+  }
+}
---
+++ lib/features/home/presentation/manager/bottom_books_cubit/bottom_books_state.dart
@@ -0,0 +1,19 @@
+import 'package:book_app_clean_arch/features/home/domain/entities/book_entity.dart';
+
+abstract class BottomBooksState {}
+
+class BottomBooksInit extends BottomBooksState {}
+
+class BottomBooksLoading extends BottomBooksState {}
+
+class BottomBooksSuccess extends BottomBooksState {
+  final List<BookEntity> books;
+
+  BottomBooksSuccess({required this.books});
+}
+
+class BottomBooksFailure extends BottomBooksState {
+  final String msg;
+
+  BottomBooksFailure({required this.msg});
+}
