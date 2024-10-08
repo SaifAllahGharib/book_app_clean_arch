cc82f37ce7debefa36f05b91bb552570c96e6373
SaifAllahGharib <saifallahgharib@gmail.com>
2024-10-08 14:00:54 +0300

    implement top books cubit

--- lib/features/home/presentation/manager/top_books_cubit/top_books_cubit.dart
+++ lib/features/home/presentation/manager/top_books_cubit/top_books_cubit.dart
@@ -1,7 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:book_app_clean_arch/core/errors/failure.dart';
+import 'package:book_app_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:book_app_clean_arch/features/home/domain/useCases/fetch_books_use_case.dart';
+import 'package:dartz/dartz.dart';
 
 import 'top_books_state.dart';
 
 class TopBooksCubit extends Cubit<TopBooksState> {
-  TopBooksCubit() : super(TopBooksInit());
+  final FetchBooksUseCase fetchBooksUseCase;
+
+  TopBooksCubit({required this.fetchBooksUseCase}) : super(TopBooksInit());
+
+  Future<void> fetchBooks({required String path}) async {
+    emit(TopBooksLoading());
+    Either<Failure, List<BookEntity>> result =
+        await fetchBooksUseCase.call(path: path);
+
+    result.fold(
+      (failure) => emit(TopBooksFailure(msg: failure.msg)),
+      (books) => emit(TopBooksSuccess(books: books)),
+    );
+  }
 }
--- lib/features/home/presentation/manager/top_books_cubit/top_books_state.dart
+++ lib/features/home/presentation/manager/top_books_cubit/top_books_state.dart
@@ -1,10 +1,16 @@
+import 'package:book_app_clean_arch/features/home/domain/entities/book_entity.dart';
+
 abstract class TopBooksState {}
 
 class TopBooksInit extends TopBooksState {}
 
 class TopBooksLoading extends TopBooksState {}
 
-class TopBooksSuccess extends TopBooksState {}
+class TopBooksSuccess extends TopBooksState {
+  final List<BookEntity> books;
+
+  TopBooksSuccess({required this.books});
+}
 
 class TopBooksFailure extends TopBooksState {
   final String msg;
