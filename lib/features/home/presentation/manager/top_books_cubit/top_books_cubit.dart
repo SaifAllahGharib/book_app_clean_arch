6cb73ce3c2fab9de519cf47ef55ad7eff9428901
SaifAllahGharib <saifallahgharib@gmail.com>
2024-10-08 13:47:07 +0300

    Add bloc & top books cubit

---
+++ lib/features/home/presentation/manager/top_books_cubit/top_books_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bloc/bloc.dart';
+
+import 'top_books_state.dart';
+
+class TopBooksCubit extends Cubit<TopBooksState> {
+  TopBooksCubit() : super(InitTopBooks());
+}
---
+++ lib/features/home/presentation/manager/top_books_cubit/top_books_state.dart
@@ -0,0 +1,9 @@
+abstract class TopBooksState {}
+
+class InitTopBooks extends TopBooksState {}
+
+class LoadingTopBooks extends TopBooksState {}
+
+class SuccessTopBooks extends TopBooksState {}
+
+class FailureTopBooks extends TopBooksState {}
